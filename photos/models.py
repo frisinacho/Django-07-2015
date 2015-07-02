096eec0ba8b97e84902cc7841225aabb910e8b28
Alberto Casero <alberto@kasfactory.net>
2015-07-03 00:28:23 +0200

    Creamos nuestro primer modelo, lo añadimos al admin y lo mostramos en la página principal

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+COPYRIGHT = 'RIG'
+COPYLEFT = 'LEF'
+CREATIVE_COMMONS = 'CC'
+
+LICENSES = (
+    (COPYRIGHT, 'Copyright'),
+    (COPYLEFT, 'Copyleft'),
+    (CREATIVE_COMMONS, 'Creative Commons')
+)
+
+class Photo(models.Model):
+
+    name = models.CharField(max_length=150)
+    url = models.URLField()
+    description = models.TextField(blank=True, null=True, default="")
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    license = models.CharField(max_length=3, choices=LICENSES)
+
+    def __unicode__(self):  # 0 param method
+        return self.name
--- photos/views.py
+++ photos/views.py
@@ -1,6 +1,16 @@
+#-*- coding: utf-8 -*-
 from django.http import HttpResponse
 from django.shortcuts import render
+from photos.models import Photo
 
 
 def home(request):
-    return HttpResponse("<strong>Hola</strong> mundo!")
+    """
+    Esta función devuelve el home de mi página
+    """
+    photos = Photo.objects.all()
+    html = '<ul>'
+    for photo in photos:
+        html += '<li>' + photo.name + '</li>'
+    html += '</ul>'
+    return HttpResponse(html)
