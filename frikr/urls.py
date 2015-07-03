b81119317bb0a5ebcb660fca893751fbb263aacf
Alberto Casero <alberto@kasfactory.net>
2015-07-04 00:30:04 +0200

    Implementamos el login y el logout Utilizamos URLs con nombre en lugar de hardcodeadas Refactorizamos las plantillas para heredar de base.html

--- frikr/urls.py
+++ frikr/urls.py
@@ -4,7 +4,11 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^$', 'photos.views.home'),
-    url(r'^photos/(?P<pk>[0-9]+)$', 'photos.views.detail')
+    # Photos URLs
+    url(r'^$', 'photos.views.home', name='photos_home'),
+    url(r'^photos/(?P<pk>[0-9]+)$', 'photos.views.detail', name='photo_detail'),
 
+    # Users URLS
+    url(r'^login$', 'users.views.login', name='users_login'),
+    url(r'^logout$', 'users.views.logout', name='users_logout')
 ]
