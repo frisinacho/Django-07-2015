d7415872447e90d38667add259018a987637fd78
Alberto Casero <alberto@kasfactory.net>
2015-07-08 00:25:19 +0200

    Nuestro primer endpoint de Django REST Framework

--- users/api.py
+++ users/api.py
@@ -1,17 +1,14 @@
 # -*- coding: utf-8 -*-
 from django.contrib.auth.models import User
-from django.http import HttpResponse
-from django.views.generic import View
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from users.serializers import UserSerializer
-from rest_framework.renderers import JSONRenderer
 
 
-class UserListAPI(View):
+class UserListAPI(APIView):
 
     def get(self, request):
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
         serialized_users = serializer.data  # lista de diccionarios
-        renderer = JSONRenderer()
-        json_users = renderer.render(serialized_users)  # lista de diccionarios -> JSON
-        return HttpResponse(json_users)
+        return Response(serialized_users)
