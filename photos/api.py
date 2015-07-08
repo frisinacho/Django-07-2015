d5223bd0504cbbb69fc3a58fc0041970413938a2
Alberto Casero <alberto@kasfactory.net>
2015-07-09 00:09:47 +0200

    Asignamos la propiedad de la foto al usuario autenticado automáticamente

--- photos/api.py
+++ photos/api.py
@@ -16,6 +16,9 @@ class PhotoListAPI(PhotosQueryset, ListCreateAPIView):
     def get_queryset(self):
         return self.get_photos_queryset(self.request)
 
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
 
 class PhotoDetailAPI(PhotosQueryset, RetrieveUpdateDestroyAPIView):
     queryset = Photo.objects.all()
