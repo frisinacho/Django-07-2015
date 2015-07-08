f0554f1fefaa7c0ba02f9e7aed3b124b8f607506
Alberto Casero <alberto@kasfactory.net>
2015-07-08 23:59:00 +0200

    Sobreescribiendo el método get_queryset, implemento un filtrado dinámico como hacía en la web con las fotos (en función del usuario muestro unas fotos u otras)

--- photos/api.py
+++ photos/api.py
@@ -1,16 +1,26 @@
 # -*- coding: utf-8 -*-
 from photos.models import Photo
 from photos.serializers import PhotoSerializer, PhotoListSerializer
+from photos.views import PhotosQueryset
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
-class PhotoListAPI(ListCreateAPIView):
+class PhotoListAPI(PhotosQueryset, ListCreateAPIView):
     queryset = Photo.objects.all()
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
     def get_serializer_class(self):
         return PhotoSerializer if self.request.method == "POST" else PhotoListSerializer
 
+    def get_queryset(self):
+        return self.get_photos_queryset(self.request)
 
-class PhotoDetailAPI(RetrieveUpdateDestroyAPIView):
+
+class PhotoDetailAPI(PhotosQueryset, RetrieveUpdateDestroyAPIView):
     queryset = Photo.objects.all()
     serializer_class = PhotoSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    def get_queryset(self):
+        return self.get_photos_queryset(self.request)
