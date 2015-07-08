129b85a09868be66826b0c62107f871a2d67592a
Alberto Casero <alberto@kasfactory.net>
2015-07-08 22:08:17 +0200

    - Implementamos endpoint de borrado de usuarios - Usamos un ModelSerializer para hacer un endpoint de listado de fotos

--- frikr/urls.py
+++ frikr/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth.decorators import login_required
+from photos.api import PhotoListAPI
 
 from photos.views import HomeView, DetailView, CreateView, PhotoListView, UserPhotosView
 from users.api import UserListAPI, UserDetailAPI
@@ -15,6 +16,9 @@ urlpatterns = [
     url(r'^photos/$', PhotoListView.as_view(), name='photos_list'),
     url(r'^photos/(?P<pk>[0-9]+)$', DetailView.as_view(), name='photo_detail'),
     url(r'^photos/new$', CreateView.as_view(), name='create_photo'),
+
+    # Photos API URLs
+    url(r'^api/1.0/photos/$', PhotoListAPI.as_view(), name='photo_list_api'),
 
     # Users URLS
     url(r'^login$', LoginView.as_view(), name='users_login'),
