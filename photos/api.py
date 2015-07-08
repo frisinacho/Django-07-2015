319e62f70b547338d4c3535d5ebfe92766fdb78a
Alberto Casero <alberto@kasfactory.net>
2015-07-08 23:13:42 +0200

    Usando dos serializers, mostramos diferentes datos en los listados y detalle de fotos

--- photos/api.py
+++ photos/api.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 from photos.models import Photo
-from photos.serializers import PhotoSerializer
+from photos.serializers import PhotoSerializer, PhotoListSerializer
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 
 class PhotoListAPI(ListCreateAPIView):
     queryset = Photo.objects.all()
-    serializer_class = PhotoSerializer
+
+    def get_serializer_class(self):
+        return PhotoSerializer if self.request.method == "POST" else PhotoListSerializer
 
 
 class PhotoDetailAPI(RetrieveUpdateDestroyAPIView):
--- photos/serializers.py
+++ photos/serializers.py
@@ -7,3 +7,9 @@ class PhotoSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Photo
+
+
+class PhotoListSerializer(PhotoSerializer):
+
+    class Meta(PhotoSerializer.Meta):
+        fields = ('id', 'name', 'url')
