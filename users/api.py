9a112b2b6f86cb65f7fdbd1879b8da2ea1941198
Ignacio Lasaosa <frisinacho@gmail.com>
2016-01-22 11:59:29 +0100

    Deleting unused imports
    Una vez que usamos 'GenericViewSet' el 'GenericAPIView' ya no se usa más

--- users/api.py
+++ users/api.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 from django.core.mail import send_mail
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
-from rest_framework.generics import GenericAPIView
 from rest_framework.viewsets import GenericViewSet
 from users.permissions import UserPermission
 from users.serializers import UserSerializer
